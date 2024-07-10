18b3f8729d4657b2877c168d10cc7cd0a361cd04
Chris Want <cjwant@gmail.com>
2024-07-10 10:20:54 -0600

    Revert locale settings for test suite to upstream values.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -59,11 +59,8 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
-  # Don't raises error for missing translations
-  # config.i18n.raise_on_missing_translations = true
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = [I18n.default_locale]
+  # Raises error for missing translations
+  config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
