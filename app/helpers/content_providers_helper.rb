68b39e1ccf610be5ff80a946078d1fabfbe46003
Chris Want <cjwant@gmail.com>
2025-04-10 15:03:38 -0600

    Randomly pick one of 10 content providers for the carousel.

--- app/helpers/content_providers_helper.rb
+++ app/helpers/content_providers_helper.rb
@@ -1,4 +1,16 @@
 # The helper for Content Providers
 module ContentProvidersHelper
   CONTENT_PROVIDERS_INFO = I18n.t 'providers.info'
+
+  def carousel_content_providers
+    provider_ids = TeSS::Config.site.dig('home_page', 'featured_providers')
+    provider_ids ||= ContentProvider.from_verified_users.
+                       where.not(image_file_size: nil).
+                       pluck(:id)
+    # Randomly select the desired number of providers ...
+    provider_ids = provider_ids.sample(TeSS::Config.site['n_provider_ids'])
+    return if provider_ids.blank?
+
+    ContentProvider.where(id: provider_ids)
+  end
 end
