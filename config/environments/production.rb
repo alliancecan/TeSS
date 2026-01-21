8692eb616c5351f9026dec49c075caa531066b7f
Chris Want <cjwant@gmail.com>
2026-01-21 13:34:17 -0700

    Some configuration of production.rb so we don't have to clobber it in CI pipeline (configured with env vars.)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,11 @@ Rails.application.configure do
   config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = if ENV['FORCE_SSL']
+                       ENV['FORCE_SSL'].to_s == 'true'
+                     else
+                       true
+                     end
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
@@ -114,10 +118,13 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  if ENV['APP_HOST']
+    config.hosts = [
+      #   "example.com",     # Allow requests from example.com
+      #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+      ENV['APP_HOST']
+    ]
+  end
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
