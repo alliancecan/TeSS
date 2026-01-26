f7980edb2be185cee45ce57b90c8e465f97a54bb
Chris Want <cjwant@gmail.com>
2026-01-26 09:56:41 -0700

    Make sure APP_HOST and localhost are permitted to do AJAX requests.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,11 +119,12 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   if ENV['APP_HOST']
-    config.hosts = [
-      #   "example.com",     # Allow requests from example.com
-      #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-      ENV['APP_HOST']
-    ]
+    config.hosts ||= []
+    config.hosts << ENV['APP_HOST']
+    config.hosts << "localhost"
+    config.hosts << ".localhost"
+    #   "example.com",     # Allow requests from example.com
+    #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   end
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
